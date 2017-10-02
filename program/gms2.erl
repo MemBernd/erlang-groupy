51c9bf780b2347f77d83159fe7adb0a025515c1f
Bernardo Gonzalez Riede <31217674+MemBernd@users.noreply.github.com>
2017-10-02 20:06:59 +0200

    3.1 working

--- program/gms2.erl
+++ program/gms2.erl
@@ -1,5 +1,6 @@
--module(gms1).
+-module(gms2).
 -compile(export_all).
+-define(timeout, 2000).
 
 leader(Id, Master, Slaves, Group) ->
     receive
@@ -9,8 +10,8 @@ leader(Id, Master, Slaves, Group) ->
             leader(Id, Master, Slaves, Group);
         {join, Wrk, Peer} ->
             Slaves2 = lists:append(Slaves, [Peer]),
-            Group2 = lists:apppend(Group, [Wrk]),
-            bcast(Id, {view, [self()|Slaves2], Group2}, Slaves),
+            Group2 = lists:append(Group, [Wrk]),
+            bcast(Id, {view, [self()|Slaves2], Group2}, Slaves2),
             Master ! {view, Group2},
             leader(Id, Master, Slaves2, Group2);
         stop ->
@@ -18,6 +19,7 @@ leader(Id, Master, Slaves, Group) ->
     end.
 
 slave(Id, Master, Leader, Slaves, Group) ->
+    erlang:monitor(process, Leader),
     receive
         {mcast, Msg} ->
             Leader ! {mcast, Msg},
@@ -31,10 +33,22 @@ slave(Id, Master, Leader, Slaves, Group) ->
         {view, [Leader|Slaves2], Group2} ->
             Master ! {view, Group2},
             slave(Id, Master, Leader, Slaves2, Group);
-        {'Down', _Ref, process, Leader, _Reason} ->
-            election(Id, master, Slaves, Group);
+        {'DOWN', _Ref, process, Leader, _Reason} ->
+            election(Id, Master, Slaves, Group);
         stop ->
             ok
+    end.
+
+election(Id, Master, Slaves, [_|Group]) ->
+    Self = self(),
+    case Slaves of
+        [Self|Rest] ->
+            bcast(Id, {view, Slaves, Group}, Rest),
+            Master ! {view, Group},
+            leader(Id, Master, Rest, Group);
+        [Leader|Rest] ->
+            erlang:monitor(process, Leader),
+            slave(Id, master, Leader, Rest, Group)
     end.
 
 start(Id) ->
@@ -54,6 +68,12 @@ init(Id, Grp, Master) ->
     receive
         {view, [Leader|Slaves], Group} ->
             Master ! {view, Group},
-            erlang:monitor(process, Leader),
             slave(Id, Master, Leader, Slaves, Group)
+        after 2000 ->
+            Master ! {error, "no reply from leader"}
     end.
+
+bcast(Id, {msg, Msg}, Slaves) ->
+    lists:foreach(fun(Slave) -> Slave ! {msg, Msg} end, Slaves);
+bcast(Id, {view, View, Group}, Slaves) ->
+    lists:foreach(fun(Slave) -> Slave ! {view, View, Group} end, Slaves).
